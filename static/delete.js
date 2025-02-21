c0e5e0e45f56218852690392d19ab0b035d3c4a5
GagaYaba <gaelle.lanic@yahoo.fr>
2025-02-21 14:54:56 +0100

    feat: nice button

--- static/delete.js
+++ static/delete.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     const deleteButtons = document.querySelectorAll('.delete-button');
-    
+    const modal = document.getElementById('confirmation-modal');
+    const confirmButton = document.getElementById('confirm-delete');
+    const cancelButton = document.getElementById('cancel-delete');
+    let pollIdToDelete = null;
+
     if (deleteButtons.length === 0) {
         console.log("Aucun bouton de suppression trouvé.");
         return;
@@ -8,15 +12,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
     deleteButtons.forEach(button => {
         button.addEventListener('click', function () {
-            const pollId = button.getAttribute('data-poll-id');
-            console.log('Poll ID:', pollId);
+            pollIdToDelete = button.getAttribute('data-poll-id');
+            console.log('Poll ID:', pollIdToDelete);
 
-            if (!pollId) {
+            if (!pollIdToDelete) {
                 alert("Erreur : ID du sondage introuvable.");
                 return;
             }
 
-            fetch(`/sondages/${pollId}`, {
+            modal.style.display = 'flex';
+        });
+    });
+
+    confirmButton.addEventListener('click', function () {
+        if (pollIdToDelete) {
+            fetch(`/sondages/${pollIdToDelete}`, {
                 method: 'DELETE',
             })
             .then(response => {
@@ -38,6 +48,12 @@ document.addEventListener("DOMContentLoaded", function () {
                 console.error('Erreur lors de la suppression:', error);
                 alert("Erreur: " + error);
             });
-        });
+        }
+        
+        modal.style.display = 'none';
+    });
+
+    cancelButton.addEventListener('click', function () {
+        modal.style.display = 'none';
     });
 });
